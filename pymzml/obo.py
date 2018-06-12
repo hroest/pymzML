b10f7fdc1b9ef8d8ef59e44c436ba55788d031f0
Hannes Roest <hroest@stanford.edu>
2018-06-12 19:13:36 -0400

    [FIX] obo file is relative to current file

--- pymzml/obo.py
+++ pymzml/obo.py
@@ -138,7 +138,8 @@ class oboTranslator(object):
 
         # Modify the root for cx_freeze
         if getattr(sys, 'frozen', False):
-            obo_root = os.path.dirname(sys._MEIPASS)
+            # obo_root = os.path.dirname(sys._MEIPASS)
+            obo_root = os.path.dirname(__file__)
         else:
             obo_root = os.path.dirname(__file__)
 
